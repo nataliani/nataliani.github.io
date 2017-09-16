eab4ed680ebe61106b22a2ec43a727cdaec45248
Natalia Markova <natka.markova.1995@mail.ru>
2017-09-16 23:32:12 +1000

    Init

---
+++ header.php
@@ -0,0 +1,31 @@
+﻿<!DOCTYPE html>
+<html lang="ru">
+  <head>
+  	<meta charset="UTF-8">
+  	<title>Резюме</title>
+	<link rel="stylesheet" type="text/css" href="/style/ovaral.css">
+    <link rel="icon" type="image/x-icon" href="image/icon.ico" />
+    <script src="/js/jquery-3.2.1.min.js"></script>
+   <script src="/js/slider.js"></script>
+    <script src="/js/menu.js"></script>
+  </head>
+  <body>
+  	<div id="header">
+		<div id="slider">
+          
+                <div id="slidewrapper">
+                    <img src="image/slider/page1.png" alt="Приветствую вас на своём ресурсе" class="slide">
+                    <img src="image/slider/page2.png" alt="Здесь вы можете познакомится с моими творческими начинаниями" class="slide">
+                    <img src="image/slider/page3.png" alt="И ознакомится с дебрями моего погружения в основы WEB-программирования" class="slide">
+                </div>
+                <div id="navBth">
+                    <input type="radio" name="navigation" class="navigation" id="1">
+                    <input type="radio" name="navigation" class="navigation" id="2">
+                    <input type="radio" name="navigation" class="navigation" id="3"> 
+                </div>
+           
+        </div>
+		
+	</div>
+	<div id="conteiner">
+		
